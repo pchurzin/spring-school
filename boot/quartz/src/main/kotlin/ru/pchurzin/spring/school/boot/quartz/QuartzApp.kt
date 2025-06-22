37641f97a2331e85b4c74826f3926f4e992a2649
Pavel Churzin <pavel@pchurzin.ru>
2025-06-22 20:43:04 +0300

    simple quartz job

--- boot/quartz/src/main/kotlin/ru/pchurzin/spring/school/boot/quartz/QuartzApp.kt
+++ boot/quartz/src/main/kotlin/ru/pchurzin/spring/school/boot/quartz/QuartzApp.kt
@@ -1,11 +1,40 @@
 package ru.pchurzin.spring.school.boot.quartz
 
+import org.quartz.*
+import org.quartz.SimpleScheduleBuilder.simpleSchedule
+import org.quartz.TriggerBuilder.newTrigger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 fun main() {
     runApplication<QuartzApp>()
 }
 
 @SpringBootApplication
-class QuartzApp
+class QuartzApp {
+
+    @Bean
+    fun jobDetail(): JobDetail =
+        JobBuilder.newJob(SimpleJob::class.java)
+            .withIdentity("simpleJob")
+            .storeDurably()
+            .build()
+
+    @Bean
+    fun simpleJobTrigger(): Trigger =
+        newTrigger()
+            .forJob(jobDetail())
+            .withIdentity("simpleJobTrigger")
+            .withSchedule(
+                simpleSchedule()
+                    .withIntervalInSeconds(5)
+                    .repeatForever())
+            .build()
+}
+
+class SimpleJob : Job {
+    override fun execute(context: JobExecutionContext) {
+        println("Hello from Quartz!")
+    }
+}
